03fa581aeb44aae3122607fa7cc75fc7773316d5
JamieH <JamieH@users.noreply.github.com>
2018-07-30 21:12:54 +0100

    Update and rename cloudflare.groovy to cloudflarepurgeAll.groovy

---
+++ vars/cloudflarepurgeAll.groovy
@@ -0,0 +1,19 @@
+#!/usr/bin/env groovy
+
+def run(String credId, String zone) {
+    
+    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'cloudflare-api',
+                    usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD'],
+                    string(credentialsId: credId, variable: 'zone'),]) {
+                        
+    
+    sh """#!/bin/bash
+    set -x
+    curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${zone}/purge_cache" \
+    -H "Content-Type:application/json" \
+    -H "X-Auth-Key:$PASSWORD" \
+    -H "X-Auth-Email:$USERNAME" \
+    --data '{"purge_everything":true}'
+    """
+    }
+}
