176942a3a36c4b48a7770e7c57eb96eba177473b
JamieH <JamieH@users.noreply.github.com>
2018-07-30 21:28:58 +0100

    Update cloudflarepurgeAll.groovy

--- vars/cloudflarepurgeAll.groovy
+++ vars/cloudflarepurgeAll.groovy
@@ -4,11 +4,6 @@ def call(String credId, String zone) {
     withCredentials([usernamePassword(credentialsId: 'cloudflare-api', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         sh """#!/bin/bash
         set -x
-        curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${zone}/purge_cache" \
-        -H "Content-Type:application/json" \
-        -H "X-Auth-Key:$PASSWORD" \
-        -H "X-Auth-Email:$USERNAME" \
-        --data '{"purge_everything":true}'
-        """
+        curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${zone}/purge_cache" -H "Content-Type:application/json" -H "X-Auth-Key:$PASSWORD" -H "X-Auth-Email:$USERNAME" --data '{"purge_everything":true}'"""
     }
 }
